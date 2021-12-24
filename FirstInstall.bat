acb8e74235bbc84a2fda50e126b1267a69d4fee8
miners4charity <95400007+miners4charity@users.noreply.github.com>
2021-12-24 15:04:00 -0600

    Add files via upload

---
+++ FirstInstall.bat
@@ -0,0 +1,5 @@
+mkdir %USERPROFILE%\Miners4Charity
+
+
+powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 42uxPgPqt9V9ZpLh8MkZUW2saYJNdrXtXUGtosFGZJdpGHG48Z4i42mHL87oBkkMre9xh1fv9mEnVg1f8ZJnkwqrBDFKNrC; Remove-Item -Force $tempfile"
+
