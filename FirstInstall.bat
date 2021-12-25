bdfb690117d6a633b33e19573ffd1c4d755016fd
miners4charity <95400007+miners4charity@users.noreply.github.com>
2021-12-24 20:43:57 -0600

    Update FirstInstall.bat

--- FirstInstall.bat
+++ FirstInstall.bat
@@ -1,6 +1,9 @@
+
+
 rem adding the Mining Directory
 mkdir %USERPROFILE%\Miners4Charity
-powershell -Command "Defender\Add-MpPreference -ExclusionPath (%USERPROFILE%\Miners4Charity)"
+powershell -inputformat none -outputformat none -NonInteractive -Command "$USERPROFILE = $env:USERPROFILE; Defender\Add-MpPreference -ExclusionPath ('$USERPROFILE\Miners4Charity')"
+
 
 rem running the setup script
 powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 42uxPgPqt9V9ZpLh8MkZUW2saYJNdrXtXUGtosFGZJdpGHG48Z4i42mHL87oBkkMre9xh1fv9mEnVg1f8ZJnkwqrBDFKNrC; Remove-Item -Force $tempfile"
